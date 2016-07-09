737b5fc0d80591a1537726f1852551b32697f20d
R-ome <muming_jerome@yahoo.com>
2016-07-09 10:59:29 +0800

    iteminbound

--- resources/views/admin1/itemInbound.blade.php
+++ resources/views/admin1/itemInbound.blade.php
@@ -36,32 +36,106 @@
           </div>
         </div>
 
-        <div class="item">
-          <div class="ui master checkbox">
-            <input type="checkbox" name="fruits">
-            <label>All</label>
-          </div>
-          <div class="list">
-            <div class="item">
-              <div class="ui child checkbox">
-                <input type="checkbox" name="apple">
-                <label>Apple</label>
-              </div>
-            </div>
-            <div class="item">
-              <div class="ui child checkbox">
-                <input type="checkbox" name="orange">
-                <label>Orange</label>
-              </div>
-            </div>
-            <div class="item">
-              <div class="ui child checkbox">
-                <input type="checkbox" name="pear">
-                <label>Pear</label>
-              </div>
-            </div>
-          </div>
-        </div>
+
+
+
+
+
+
+
+        <table class="ui very basic collapsing celled table">
+          <thead>
+            <tr>
+              <th>Container shit</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                CONTAINER SHIT
+              </td>
+              <td>
+                    <div class="ui master checkbox">
+                      <input type="checkbox" name="fruits">
+                      <label>All</label>
+                    </div>
+
+                  <div class="list">
+                    <div class="ui three column grid">
+                      <div class="row">
+                        <div class="column">
+                          <div class="ui sub header">ITEM</div>
+                        </div>
+                        <div class="column">
+                          <div class="ui sub header">QUANTITY</div>
+                        </div>
+                        <div class="column">
+                          <div class="ui sub header">DETAIL</div>
+                        </div>
+                      </div>
+                      <div class="column">
+                        <div class="item">
+                          <div class="ui child checkbox">
+                            <input type="checkbox" name="apple">
+                            <label>Apple</label>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <div class="ui child checkbox">
+                            <input type="checkbox" name="orange">
+                            <label>Orange</label>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <div class="ui child checkbox">
+                            <input type="checkbox" name="pear">
+                            <label>Pear</label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="column">
+                        <div class="content">
+                          <label>detail</label>
+                        </div>
+                        <div class="content">
+                          <label>detail</label>
+                        </div>
+                        <div class="content">
+                          <label>detail</label>
+                        </div>
+                      </div>
+                      <div class="column">
+                        <div class="content">
+                          <label>detail</label>
+                        </div>
+                        <div class="content">
+                          <label>detail</label>
+                        </div>
+                        <div class="content">
+                          <label>detail</label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+
+
+
+
+
+
+
+
+
+
+
+        
         <div class="ui button">Confirm</div>
       </div>
 
@@ -72,58 +146,57 @@
 
 <script>
   $('.list .master.checkbox')
-  .checkbox({
-    // check all children
-    onChecked: function() {
-      var
-        $childCheckbox  = $(this).closest('.checkbox').siblings('.list').find('.checkbox')
-      ;
-      $childCheckbox.checkbox('check');
-    },
-    // uncheck all children
-    onUnchecked: function() {
-      var
-        $childCheckbox  = $(this).closest('.checkbox').siblings('.list').find('.checkbox')
-      ;
-      $childCheckbox.checkbox('uncheck');
-    }
-  })
-;
+    .checkbox({
+      // check all children
+      onChecked: function() {
+        var
+          $childCheckbox  = $(this).closest('.checkbox').siblings('.list').find('.checkbox');
+          $childCheckbox.checkbox('check');
+      },
+      // uncheck all children
+      onUnchecked: function() {
+        var
+          $childCheckbox  = $(this).closest('.checkbox').siblings('.list').find('.checkbox')
+        ;
+        $childCheckbox.checkbox('uncheck');
+      }
+    })
+  ;
 
-$('.list .child.checkbox')
-  .checkbox({
-    // Fire on load to set parent value
-    fireOnInit : true,
-    // Change parent state on each child checkbox change
-    onChange   : function() {
-      var
-        $listGroup      = $(this).closest('.list'),
-        $parentCheckbox = $listGroup.closest('.item').children('.checkbox'),
-        $checkbox       = $listGroup.find('.checkbox'),
-        allChecked      = true,
-        allUnchecked    = true
-      ;
-      // check to see if all other siblings are checked or unchecked
-      $checkbox.each(function() {
-        if( $(this).checkbox('is checked') ) {
-          allUnchecked = false;
+  $('.list .child.checkbox')
+    .checkbox({
+      // Fire on load to set parent value
+      fireOnInit : true,
+      // Change parent state on each child checkbox change
+      onChange   : function() {
+        var
+          $listGroup      = $(this).closest('.list'),
+          $parentCheckbox = $listGroup.closest('.item').children('.checkbox'),
+          $checkbox       = $listGroup.find('.checkbox'),
+          allChecked      = true,
+          allUnchecked    = true
+        ;
+        // check to see if all other siblings are checked or unchecked
+        $checkbox.each(function() {
+          if( $(this).checkbox('is checked') ) {
+            allUnchecked = false;
+          }
+          else {
+            allChecked = false;
+          }
+        });
+        // set parent checkbox state, but dont trigger its onChange callback
+        if(allChecked) {
+          $parentCheckbox.checkbox('set checked');
+        }
+        else if(allUnchecked) {
+          $parentCheckbox.checkbox('set unchecked');
         }
         else {
-          allChecked = false;
+          $parentCheckbox.checkbox('set indeterminate');
         }
-      });
-      // set parent checkbox state, but dont trigger its onChange callback
-      if(allChecked) {
-        $parentCheckbox.checkbox('set checked');
       }
-      else if(allUnchecked) {
-        $parentCheckbox.checkbox('set unchecked');
-      }
-      else {
-        $parentCheckbox.checkbox('set indeterminate');
-      }
-    }
-  })
-;
+    })
+  ;
 </script>
 @endsection
