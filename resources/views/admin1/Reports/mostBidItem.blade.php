af32f46bc98ea6aa475ac98f24988ccf790c14e4
ewankobkt <angelo.alimboyao@gmail.com>
2016-10-10 17:07:48 +0800

    added top 10 in most bid

--- resources/views/admin1/Reports/mostBidItem.blade.php
+++ resources/views/admin1/Reports/mostBidItem.blade.php
@@ -207,8 +207,14 @@ window.chart = new Highcharts.Chart({
                 if(!is_null($item)){
                     $ctr = count($item);
                     echo "{name: ' ', data:[";
-                    for ($i=0; $i<$ctr; $i++) {
-                        echo $item[$i][1].",";
+                    if($ctr>=10){
+                        for ($i=0; $i<10; $i++) {
+                            echo $item[$i][1].",";
+                        }
+                    } else{
+                        for ($i=0; $i<$ctr; $i++) {
+                            echo $item[$i][1].",";
+                        }
                     }
                     echo "]}";
                 } else{
@@ -217,67 +223,6 @@ window.chart = new Highcharts.Chart({
             ?>
     ]
 });
-
-/*$(function() {
-    $('#container').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Most Bid Items'
-        },
-        xAxis: {
-            categories: [
-                <?php
-                    if(!is_null($item)){
-                        $ctr = count($item);
-                        for ($i=0; $i<$ctr; $i++) { 
-                            echo "'".$item[$i][0]."',";
-                        }
-                    }
-                ?>
-            ],
-            crosshair: true
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Number of Bids'
-            }
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0"></td>' +
-                '<td style="padding:0"><b>Count: {point.y:.0f}</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [
-            <?php
-                //echo "alert(JSON.stringify(".$item."))";
-                if(!is_null($item)){
-                    $ctr = count($item);
-                    for ($i=0; $i<$ctr; $i++) {
-                        echo "{name: '".$item[$i][0]."',data:[".$item[$i][1]."]}";
-                        if($i+1!=$ctr){
-                            echo ",";
-                        }
-                    }
-                } else{
-                    echo "name: 'Nothing to show'";
-                }
-            ?>
-        ]
-    });
-});
-*/
 
 //startDate
       var date = new Date();
